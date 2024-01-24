f3cd411d7ebb918d1424b2479f68faa962725d55
IgorPanasik <panasik.igor.com@gmail.com>
2024-01-24 21:02:25 +0300

    Updates

--- assets/weather-images-js/weatherImages.js
+++ assets/weather-images-js/weatherImages.js
@@ -6,4 +6,12 @@ export const weatherImages = {
 	fog: 'src/assets/icon-weather/mist-svgrepo-com.svg',
 	'clear sky': 'src/assets/icon-weather/sun-svgrepo-com.svg',
 	clouds: 'src/assets/icon-weather/cloud-svgrepo-com.svg',
+	drizzle: 'src/assets/icon-weather/cloud-with-rain-svgrepo-com.svg',
+	rain: 'src/assets/icon-weather/cloud-with-rain-svgrepo-com.svg',
+	tornado: 'src/assets/icon-weather/tornado-svgrepo-com.svg',
+	dust: 'src/assets/icon-weather/storm-dust-svgrepo-com.svg',
+	sand: 'src/assets/icon-weather/storm-dust-svgrepo-com.svg',
+	smoke: 'src/assets/icon-weather/weather-icons-39-svgrepo-com.svg',
+	ash: 'src/assets/icon-weather/storm-dust-svgrepo-com.svg',
+	squall: 'src/assets/icon-weather/weather-squalls-svgrepo-com.svg',
 };
