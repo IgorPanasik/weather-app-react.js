1a3d1a56924e9113f2f79277c76abc6b50ea1cf8
IgorPanasik <panasik.igor.com@gmail.com>
2024-01-24 20:43:28 +0300

    Updates

---
+++ assets/weather-images-js/weatherImages.js
@@ -0,0 +1,9 @@
+export const weatherImages = {
+	'thunderstorm with rain':
+		'src/assets/icon-weather/cloud-with-lightning-and-rain-svgrepo-com.svg',
+	thunderstorm: 'src/assets/icon-weather/cloud-with-lightning-svgrepo-com.svg',
+	snow: 'src/assets/icon-weather/cloud-with-snow-svgrepo-com.svg',
+	fog: 'src/assets/icon-weather/mist-svgrepo-com.svg',
+	'clear sky': 'src/assets/icon-weather/sun-svgrepo-com.svg',
+	clouds: 'src/assets/icon-weather/cloud-svgrepo-com.svg',
+};
